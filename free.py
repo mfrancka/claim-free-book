b87df4a338bf10c12d84b4c3472e4ac1adb26088
Mariusz Francka <mfrancka@users.noreply.github.com>
2019-02-12 16:07:55 +0100

    Fix parser and sending message (#1)
    * fix(all): fix script to work with new webpage
    
    New parser.
    
    * add recipients by environment
    
    Add recipients by environmental variable.
    
    * Issue fixed
    
    It parse new page and send messages to recipients from
    environment variable.

--- free.py
+++ free.py
@@ -1,47 +1,65 @@
-from bs4 import BeautifulSoup
 from email.mime.text import MIMEText
 import os
 import requests
 import smtplib
+import datetime
+from dotenv import load_dotenv
 
+class Notifier:
+    def __init__(self, host, username, password, email_address):
+        self.host = host
+        self.username =  username
+        self.password = password
+        self.email_address = email_address
 
-def send_by_email(subject, message):
-    host = os.environ['MAIL_SMTP_URL']
-    username = os.environ['MAIL_USERNAME']
-    password = os.environ['MAIL_PASSWORD']
-    email_address = os.environ['MAIL_ADDRESS']
+    def send_by_email(self, subject, message, recipients):
+        msg = MIMEText('Book of the day is: {}'.format(message))
+        msg['Subject'] = '"{} - new book"'.format(subject)
+        msg['From'] = self.email_address
 
-    msg = MIMEText('Book of the day is: {}'.format(message))
-    msg['Subject'] = '"{} - new book"'.format(subject)
-    msg['From'] = email_address
-
-    s = smtplib.SMTP(host, 587)
-    s.starttls()
-    s.login(username, password)
-    for address in ['your_email_address', ]:
-        del msg['To']
-        msg['To'] = address
-        print(msg['To'])
-        # Send the message via our own SMTP server.
-        s.send_message(msg)
-        s.rset()
-    s.quit()
+        s = smtplib.SMTP(self.host, 587)
+        s.starttls()
+        s.login(self.username, self.password)
+        for address in recipients:
+            del msg['To']
+            msg['To'] = address
+            print(msg['To'])
+            # Send the message via our own SMTP server.
+            s.send_message(msg)
+            s.rset()
+        s.quit()
 
 
 def main():
-    page = requests.get('https://www.packtpub.com/packt/offers/free-learning',
+    load_dotenv()
+    host = os.environ['MAIL_SMTP_HOST']
+    username = os.environ['MAIL_USERNAME']
+    password = os.environ['MAIL_PASSWORD']
+    email_address = os.environ['MAIL_ADDRESS']
+    recipients = os.environ['RECIPIENTS'].split(',')
+
+    (book_title, description) = get_book()
+    notifier = Notifier(host,username,password,email_address)
+    print(notifier.send_by_email(book_title,description, recipients))
+
+def get_book():
+    today = datetime.datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
+    tomorrow = today + datetime.timedelta(days=1) 
+    page = requests.get('https://services.packtpub.com/free-learning-v1/offers?dateFrom={}Z&dateTo={}Z'.format(today.isoformat(), tomorrow.isoformat()),
                          headers={'User-Agent':'curl'})
 
-    soup = BeautifulSoup(page.content, 'html.parser')
-    book_title = soup.find_all('div', class_='dotd-title')[0].find('h2')\
-                               .next_element.lstrip().rstrip()
-    description = soup.div.find(class_='dotd-main-book-summary')\
-                      .find_all('div')[2].get_text().rstrip().lstrip()
+    desc = requests.get('https://www.googleapis.com/books/v1/volumes?q=isbn:{}'.format(page.json()['data'][0]['productId']),
+                         headers={'User-Agent':'curl'})
+
+    description = ''
+    book_title = ''
+    for book in desc.json()['items']:
+        book_title += book['volumeInfo']['title']
+        description += book['volumeInfo']['description']
 
     description += "\n\n https://www.packtpub.com/packt/offers/free-learning?from=block"
-    print(description)
-    print(send_by_email(book_title, description))
-    return book_title
+    print(book_title)
+    return (book_title, description)
 
 if __name__ == '__main__':
     main()
